b03c702fcaace15a75d5c897025a4606c2e91d3f
warrenmacdonald <warrenmacdonald@outlook.com>
2017-03-15 14:41:46 +0000

    Fixed mallocs.

--- hc.c
+++ hc.c
@@ -29,7 +29,7 @@ void secretKey(int **SKey, int **invSKey, int matrix_dims) {
     for (i = 0; i < 4; i++) {
         for (j = 0; j < 4; j++) {
             SKey[0][i*matrix_dims + j] = xSKey[i][j]; 
-            invSKey[i*matrix_dims + j] = &xinvSKey[i][j];
+            invSKey[0][i*matrix_dims + j] = xinvSKey[i][j];
             printf("%d:  %d        %d   %d\n", i*matrix_dims + j, SKey[0][i*matrix_dims + j], SKey[0][i*matrix_dims+j], xSKey[i][j]);
         }
     }
@@ -39,15 +39,13 @@ void pad_msg(char *msg, int matrix_dims) {
     int i;
     unsigned int msg_len = strlen(msg);
     int matrix_size = matrix_dims*matrix_dims;
-    if (msg_len % (matrix_size) != 0) {
-        int extra_chars = matrix_size - msg_len%matrix_size;
-        char space[extra_chars];
-        for (i = 0; i < extra_chars; i++) {
-            space[i] = ' ';
-        }
-        strcat(msg, space);
+    int extra_chars = matrix_size - msg_len%matrix_size;
+
+    char *space = malloc(extra_chars);
+    for (i = 0; i < extra_chars; i++) {
+        space[i] = ' ';
     }
-    printf("padded\n");
+    strcat(msg, space);
 }
 
 void encode_msg(char *msg, int *msg_vector, int matrix_dims) {
@@ -56,7 +54,6 @@ void encode_msg(char *msg, int *msg_vector, int matrix_dims) {
 
     while (msg[i] != '\0') {
         msg_vector[i] = (int)(msg[i]);
-//        printf("%c %d", msg[i], msg_vector[i]);
         i++;
     }
 }
@@ -82,10 +79,13 @@ int main(int argc, char *argv[]) {
             // printf("%s", msg_file_text);
             msg = msg_file_text;
         } else {
-            msg = argv[1];
+            msg = malloc(strlen(argv[1]) + 15);
+            strcpy(msg, argv[1]);
         }
     } else {
-        msg = "All work and no play makes Jack a dull boy.";
+        const char* jack_msg = "All work and no play makes Jack a dull boy.";
+        msg = malloc(strlen(jack_msg));
+        strcpy(msg, jack_msg);
     }
     char *ascii_dict = malloc(128);
     asciiMap(ascii_dict);
@@ -94,56 +94,48 @@ int main(int argc, char *argv[]) {
     int matrix_dims = 4;
     int matrix_size = matrix_dims*matrix_dims;
     // int **SKey; 
-    int **SKey = malloc(matrix_dims * sizeof(int *));
+    
+
+    // malloc() for 4 (int*) pointers to 4 rows, also malloc'd (int)
     int z;
+    int **SKey = malloc(matrix_dims * sizeof(int *));
     for (z = 0; z < matrix_dims; z++) {
         SKey[z] = malloc(matrix_dims * sizeof(int));
     }
-    //SKey = malloc(matrix_dims*sizeof(int *) + matrix_dims*matrix_dims*sizeof(int));
-    // int invSKey; 
     int **invSKey = malloc(matrix_dims * sizeof(int *));
     for (z = 0; z < matrix_dims; z++) {
         invSKey[z] = malloc(matrix_dims * sizeof(int));
     }
-//    invSKey = malloc(matrix_dims*sizeof(int *) + matrix_dims*matrix_dims*sizeof(int));
-    
+   
+    int msg_size = strlen(msg);
+    printf("STRLEN:  %d     MESSAGE SIZE: %d", strlen(msg), msg_size);
+    // populate secret and inverse secret keys 
     secretKey(SKey, invSKey, matrix_dims);
-    int msg_len = strlen(msg) - 1;
-    char* hello;
+    
     int i, j;
-
+    printf("%s", msg);
     for (i = 0; i < 4; i++) {
         for (j = 0; j < 4; j++) {
             printf("Skey: %d\n", SKey[0][i*4 + j]);
         }
     }
-
-
-/*
-    for (i = 0; i < 4; i++) {
-        for (j = 0; j < 4; j++) {
-            printf("SKey:%d     %d\n", **SKey[i*4 + j], &SKey[0][i*4 + j]);
-        }
-    }
-/*
     // pad message if not divisible by matrix_dims^2
-    pad_msg(msg, matrix_dims);   
-   /* 
+    if (msg_size % matrix_size != 0) {
+        pad_msg(msg, matrix_dims); 
+    }
+    printf("%s", msg); 
+ 
     int *msg_vector = malloc(strlen(msg)*sizeof(int));
-   
     // message encoded, results stored in msg_vector 
     encode_msg(msg, msg_vector, matrix_dims);
     printf("ENCODED!\n");
     int msg_vector_list[strlen(msg)/matrix_size][matrix_size];
-    /*
     for (i = 0; i < msg_size%matrix_size; i++) {
         for (j = 0; j < matrix_size; j++) {
             msg_vector_list[i][j] = msg[i*matrix_size + j];
             printf("%d", msg_vector_list[i][j], "\n");
         }
     }
-    */
-
 
 
 
